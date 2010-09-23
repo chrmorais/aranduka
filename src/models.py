de472973cc3636843ef826e67d15ff0984a314ec
Roberto Alsina <ralsina@netmanagers.com.ar>
2010-09-23 09:51:21 -0300

    Added Book.cover() method to get the path to a book's cover file

--- src/models.py
+++ src/models.py
@@ -59,7 +59,14 @@ class Book (Entity):
         self.files.append(f)
         print "F2", self.files
         session.commit()
-        
+
+    def cover(self):
+        """Returns the path for the cover image if available, or the
+        default nocover picture"""
+        coverfn = os.path.join(utils.COVERPATH,"%s.jpg"%(self.id))
+        if os.path.isfile(coverfn):
+            return coverfn
+        return os.path.join(utils.SCRIPTPATH,"nocover.png")
 
     def fetch_cover(self, url = None):
         """Downloads and stores a cover for this book, if possible.
