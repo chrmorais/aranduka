b2461852c2dccf0f5cf6347fe1cb252ff0a2b302
Roberto Alsina <ralsina@netmanagers.com.ar>
2010-09-24 18:27:48 -0300

    Show the UI for the RSS feeds plugin

--- src/plugins/rss/__init__.py
+++ src/plugins/rss/__init__.py
@@ -1,6 +1,16 @@
 from pluginmgr import BookStore
 from PyQt4 import QtCore, QtGui, uic
 import config
+import os
+
+class RSSWidget(QtGui.QWidget):
+    def __init__(self, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+        uifile = os.path.join(
+            os.path.abspath(
+                os.path.dirname(__file__)),'store.ui')
+        uic.loadUi(uifile, self)
+        self.ui = self
 
 class RSSStore(BookStore):
     """Fetch RSS feeds as ePub"""
@@ -11,6 +21,7 @@ class RSSStore(BookStore):
     def __init__(self):
         print "INIT:", self.title
         self.widget = None
+        self.w = None
 
     def treeItem(self):
         """Returns a QTreeWidgetItem representing this
@@ -19,6 +30,21 @@ class RSSStore(BookStore):
 
     def setWidget (self, widget):
         self.widget = widget
+
+    def operate(self):
+        "Show the store"
+        if not self.widget:
+            print "Call setWidget first"
+            return
+        self.widget.title.setText(self.title)
+        if not self.w:
+            self.w = RSSWidget()
+            self.pageNumber = self.widget.stack.addWidget(self.w)
+        self.widget.stack.setCurrentIndex(self.pageNumber)
+
+    showGrid = operate
+    showList = operate
+
 
     @QtCore.pyqtSlot()
     def doSearch(self, *args):
