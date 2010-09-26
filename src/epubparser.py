c371256f3945b3aaf4f4fbe79732337be0b0dde1
Roberto Alsina <ralsina@netmanagers.com.ar>
2010-09-25 22:31:14 -0300

    Initial refactoring

---
+++ src/epubparser.py
@@ -0,0 +1,66 @@
+import zipfile
+from elementtree.ElementTree import XML
+
+
+class EpubDocument(object):
+    """A class that parses and provides
+    data about an ePub file"""
+
+    def __init__(self, fname):
+        # This is done according to this:
+        # http://stackoverflow.com/questions/1388467/reading-epub-format
+
+        print "Opening:", fname
+        self.book = zipfile.ZipFile(fname, "r")
+        f = self.book.open('META-INF/container.xml')
+        self.container = XML(f.read())
+        f.close()
+        roots = self.container.findall('.//{urn:oasis:names:tc:opendocument:xmlns:container}rootfile')
+        self.roots = []
+        for r in roots:
+            self.roots.append(r.attrib['full-path'])
+        opf = self.book.open(self.roots[0])
+        self.basepath = os.path.dirname(self.roots[0])+"/"
+        if self.basepath == '/':
+            self.basepath=""
+        print "BASEPATH:", self.basepath
+
+        data = opf.read()
+        self.opf=XML(data)
+        opf.close()
+        self.manifest = self.opf.find('{http://www.idpf.org/2007/opf}manifest')
+        self.manifest_dict = {}
+        for elem in self.manifest.findall('{http://www.idpf.org/2007/opf}item'):
+            self.manifest_dict[elem.attrib['id']]=self.basepath+elem.attrib['href']
+
+        self.spine = self.opf.find('{http://www.idpf.org/2007/opf}spine')
+
+        self.toc_id = self.spine.attrib['toc']
+        self.toc_fn = self.manifest_dict[self.toc_id]
+        f = self.book.open(self.toc_fn)
+        data = f.read()
+        self.toc = XML(data)
+        self.navmap = self.toc.find('{http://www.daisy.org/z3986/2005/ncx/}navMap')
+        # FIXME: support nested navpoints
+        self.navpoints = self.navmap.findall('.//{http://www.daisy.org/z3986/2005/ncx/}navPoint')
+        self.tocentries = []
+        for np in self.navpoints:
+            label = np.find('{http://www.daisy.org/z3986/2005/ncx/}navLabel').find('{http://www.daisy.org/z3986/2005/ncx/}text').text
+            content = np.find('{http://www.daisy.org/z3986/2005/ncx/}content').attrib['src']
+            if label and content:
+                self.tocentries.append([label, content])
+
+        self.itemrefs = self.spine.findall('{http://www.idpf.org/2007/opf}itemref')
+        self.spinerefs = [ self.manifest_dict[item.attrib['idref']][len(self.basepath):] for item in self.itemrefs ]
+
+    def getData(self, path):
+        """Return the contents of a file in the document"""
+
+        path = "%s%s"%(self.basepath,path)
+        try:
+            f = self.book.open(path)
+        except KeyError: #File missing in the zip
+            return []
+        data = f.read()
+        f.close()
+        return data
--- src/epubviewer.py
+++ src/epubviewer.py
@@ -1,14 +1,13 @@
 from PyQt4 import QtNetwork, QtCore, QtGui, uic
 import os, sys
-import zipfile
-from elementtree.ElementTree import XML
-
-
+from epubparser import EpubDocument
 
 class Main(QtGui.QMainWindow):
     def __init__(self, fname):
         QtGui.QMainWindow.__init__(self)
 
+        self.epub = EpubDocument(fname)
+        
         uifile = os.path.join(
             os.path.abspath(
                 os.path.dirname(__file__)),'epubviewer.ui')
@@ -16,63 +15,18 @@ class Main(QtGui.QMainWindow):
         self.ui = self
         self.addAction(self.actionPageDown)
 
-        # This is done according to this:
-        # http://stackoverflow.com/questions/1388467/reading-epub-format
 
-        print "Opening:", fname
-        self.book = zipfile.ZipFile(fname, "r")
-        f = self.book.open('META-INF/container.xml')
-        self.container = XML(f.read())
-        f.close()
-        roots = self.container.findall('.//{urn:oasis:names:tc:opendocument:xmlns:container}rootfile')
-        self.roots = []
-        for r in roots:
-            self.roots.append(r.attrib['full-path'])
-        opf = self.book.open(self.roots[0])
-        self.basepath = os.path.dirname(self.roots[0])+"/"
-        if self.basepath == '/':
-            self.basepath=""
-        print "BASEPATH:", self.basepath
-            
-        data = opf.read()
-        self.opf=XML(data)
-        opf.close()
-        self.manifest = self.opf.find('{http://www.idpf.org/2007/opf}manifest')
-        self.manifest_dict = {}
-        for elem in self.manifest.findall('{http://www.idpf.org/2007/opf}item'):
-            self.manifest_dict[elem.attrib['id']]=self.basepath+elem.attrib['href']
-
-        self.spine = self.opf.find('{http://www.idpf.org/2007/opf}spine')
-
-        self.toc_id = self.spine.attrib['toc']
-        self.toc_fn = self.manifest_dict[self.toc_id]
-        f = self.book.open(self.toc_fn)
-        data = f.read()
-        self.toc = XML(data)
-        self.navmap = self.toc.find('{http://www.daisy.org/z3986/2005/ncx/}navMap')
-        # FIXME: support nested navpoints
-        self.navpoints = self.navmap.findall('.//{http://www.daisy.org/z3986/2005/ncx/}navPoint')
-        self.tocentries = []
-        for np in self.navpoints:
-            label = np.find('{http://www.daisy.org/z3986/2005/ncx/}navLabel').find('{http://www.daisy.org/z3986/2005/ncx/}text').text
-            content = np.find('{http://www.daisy.org/z3986/2005/ncx/}content').attrib['src']
-            if label and content:
-                self.tocentries.append([label, content])
-
-        for l,c in self.tocentries:
+        for l,c in self.epub.tocentries:
             item = QtGui.QListWidgetItem(l)
             item.contents = c
             self.chapters.addItem(item)
 
-        self.itemrefs = self.spine.findall('{http://www.idpf.org/2007/opf}itemref')
-        self.spinerefs = [
-                self.manifest_dict[item.attrib['idref']][len(self.basepath):] for item in self.itemrefs
-            ]
+
         self.old_manager = self.view.page().networkAccessManager()
         self.new_manager = NetworkAccessManager(self.old_manager, self)
         self.view.page().setNetworkAccessManager(self.new_manager)
 
-        self.openPath(self.spinerefs[0])
+        self.openPath(self.epub.spinerefs[0])
         
     @QtCore.pyqtSlot()
     def on_actionPageDown_triggered(self):
@@ -81,30 +35,21 @@ class Main(QtGui.QMainWindow):
             frame.scrollPosition().y():
                 # Find where on the spine we are
                 curSpineRef= unicode(frame.url().toString())[12:]
-                curIdx = self.spinerefs.index(curSpineRef)
+                curIdx = self.epub.spinerefs.index(curSpineRef)
                 if curIdx < len(self.spinerefs):
-                    self.openPath(self.spinerefs[curIdx+1])
+                    self.openPath(self.epub.spinerefs[curIdx+1])
         else:
             frame.scroll(0,self.view.height())
         
     def on_chapters_itemClicked(self, item):
         self.openPath(item.contents)
 
-    def getData(self, path):
-        path = "%s%s"%(self.basepath,path)
-        try:
-            f = self.book.open(path)
-        except KeyError: #File missing in the zip
-            return []
-        data = f.read()
-        f.close()
-        return data
 
     def openPath(self, path, fragment=None):
         print "Opening:", path
         if "#" in path:
             path, fragment = path.split('#',1)
-        xml = self.getData(path)
+        xml = self.epub.getData(path)
         self.view.page().mainFrame().setHtml(xml,QtCore.QUrl("epub://book/"+path))
         if fragment:
             pass
@@ -116,16 +61,13 @@ class Main(QtGui.QMainWindow):
             path = unicode(url.path())
             self.openPath(path, frag)
 
-    def unsupported(self, *args):
-        print "UNSUP:", args
-
 class DownloadReply(QtNetwork.QNetworkReply):
 
     def __init__(self, parent, url, operation, w):
         self._w = w
         print"DR:", url, url.path()
         QtNetwork.QNetworkReply.__init__(self, parent)
-        self.content = self._w.getData(unicode(url.path())[1:])
+        self.content = self._w.epub.getData(unicode(url.path())[1:])
         self.offset = 0
 
         self.setHeader(QtNetwork.QNetworkRequest.ContentTypeHeader, QtCore.QVariant("application/binary"))
