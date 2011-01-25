1f438b9c24efa6f386d0daca88c0ec777b464c12
Andrés Gattinoni <andresgattinoni@gmail.com>
2011-01-25 10:12:32 -0300

    Fixing var name on ui.path()

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -4,6 +4,6 @@ def path (filename):
     """Returns the full path to an .ui file within
        the module's folder"""
     path = os.path.join(os.path.dirname(__file__), filename)
-    if not (os.path.isfile(path) and os.access(filename, os.R_OK)):
+    if not (os.path.isfile(path) and os.access(path, os.R_OK)):
         raise Exception("The file %s does not exist or is not readable"%filename)
     return path
