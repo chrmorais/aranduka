970d0a66cb8438d3c7b14219813982f867f383c2
Andrés Gattinoni <andresgattinoni@gmail.com>
2011-01-25 10:07:39 -0300

    Adding exception to ui.path() in case the file doesn't exist or isn't readable

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
-import os.path
+import os
 def path (filename):
-    return os.path.join(os.path.dirname(__file__), filename)
+    """Returns the full path to an .ui file within
+       the module's folder"""
+    path = os.path.join(os.path.dirname(__file__), filename)
+    if not (os.path.isfile(path) and os.access(filename, os.R_OK)):
+        raise Exception("The file %s does not exist or is not readable"%filename)
+    return path
