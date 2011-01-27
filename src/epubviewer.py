b2f8e8f46225c0f4f979ee34ef95ac56227f89d8
roberto.alsina <devnull@localhost>
2011-01-26 23:55:36 -0300

    Highlight the current chapter

--- src/epubviewer.py
+++ src/epubviewer.py
@@ -38,6 +38,7 @@ class Main(QtGui.QMainWindow):
                 curSpineRef= unicode(frame.url().toString())[12:]
                 curIdx = self.epub.spinerefs.index(curSpineRef)
                 if curIdx < len(self.epub.spinerefs):
+                    self.chapters.setCurrentRow(curIdx)
                     self.openPath(self.epub.spinerefs[curIdx+1])
         else:
             frame.scroll(0,self.view.height())
