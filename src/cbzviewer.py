73b222ccb401ebc1d02ece3109fe8ed49ee5fae7
roberto.alsina <devnull@localhost>
2011-02-01 22:48:10 -0300

    Switch to 'width-adjusted' view by default

--- src/cbzviewer.py
+++ src/cbzviewer.py
@@ -4,6 +4,10 @@ from cbzparser import CBZDocument
 import ui
 
 class Main(QtGui.QMainWindow):
+
+    adjWidthTpl = u"<img src=\"book:///%s\" width='100%%'>"
+    normalWidthTpl = u"<img src=\"book:///%s\">"
+
     def __init__(self, fname):
         QtGui.QMainWindow.__init__(self)
 
@@ -28,6 +32,7 @@ class Main(QtGui.QMainWindow):
         self.openPath(self.doc.tocentries[0])
         self.actionClose.triggered.connect(self.close)
         self.actionShow_Contents.toggled.connect(self.chapters.setVisible)
+        
         
     @QtCore.pyqtSlot()
     def on_actionPageDown_triggered(self):
@@ -73,7 +78,7 @@ class Main(QtGui.QMainWindow):
         path = QtCore.QUrl.fromPercentEncoding(path)
         if self.cur_path <> path:
             self.cur_path = path
-            self.view.page().mainFrame().setHtml(u"<img src=\"book:///%s\">"%path, QtCore.QUrl("epub://book/"+path))
+            self.view.page().mainFrame().setHtml(self.adjWidthTpl%path, QtCore.QUrl("epub://book/"+path))
                 
     def javascript(self, string, typ=None):
         ans = self.view.page().mainFrame().evaluateJavaScript(string)
